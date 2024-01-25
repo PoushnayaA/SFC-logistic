6f83f9870bde8cb844e6c2d62c6cb4106efd1380
PoushnayaA <poushnaya@bk.ru>
2024-01-25 04:58:11 +0300

    Add main page

--- source/js/main.js
+++ source/js/main.js
@@ -8,14 +8,34 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Utils
   // ---------------------------------
+  const languageButton = document.querySelector('.navigation-language').querySelector('button');
+  const languagesList = document.querySelector('.navigation-language__list');
+  if (languageButton) {
+    languageButton.addEventListener('click', function () {
+      languageButton.classList.toggle('active');
+      languagesList.classList.toggle('navigation-language__list--active');
+    })
+    const language = languagesList.querySelectorAll('li');
+    language.forEach(element => {
+      element.addEventListener('click', function () {
+        languagesList.classList.remove('navigation-language__list--active');
+        languageButton.dataset.value = element.dataset.language;
+        languageButton.querySelector('span').textContent = element.textContent;
+      })
+    });
+  }
+
+  const navigationOpenButton = document.querySelector('.navigation').querySelector('button');
+  if (navigationOpenButton) {
+    navigationOpenButton.addEventListener('click', function () {
+      document.querySelector('.navigation__list').classList.toggle('navigation__list--active');
+      navigationOpenButton.classList.toggle('active');
+      document.querySelector('.wrapper').classList.toggle('wrapper--dark');
+    })
+  }
 
   iosVhFix();
 
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
     const form = new Form();
@@ -23,28 +43,3 @@ window.addEventListener('DOMContentLoaded', () => {
     form.init();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используется matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
